68035ccf16bd3f995e67c9242c22c0c5907e14b4
Kevin Liu <nivekuil@ucsd.edu>
2015-09-05 05:15:43 -0700

    Send email at text route

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_mail import Message, Mail
 
 app = Flask(__name__)
+mail = Mail()
 
 @app.route("/")
 def main():
@@ -8,9 +10,12 @@ def main():
 
 @app.route("/text")
 def text():
-    # email 6266026651@txt.att.net
 
-    pass
+    # email 6266026651@txt.att.net
+    msg = Message("Hello", sender="nivekuil@gmail.com",
+                  recipients="6266026651@txt.att.net")
+
+    mail.send()
 
 if __name__ == "__main__":
     app.run()
