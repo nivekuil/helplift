6786711b26548eccce070cf069fe5e17a1c1426b
Kevin Liu <nivekuil@ucsd.edu>
2015-09-05 12:54:41 -0700

    Convert lat lon to a street address

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, render_template
+from geopy.geocoders import Bing
 import smtplib
 
 FROM = "pennapps123@gmail.com"
 
 app = Flask(__name__)
+geolocator = Bing("AlqYZzlB6mXiSQCMUK9Wum48tQFs6pN0sG6kD43mmtS61jRz7mg72E5QQg5JBJO-")
 
 @app.route("/")
 def main():
@@ -21,24 +23,31 @@ def text():
     latitude = request.args.get('lat')
     longitude = request.args.get('lon')
 
+    location = geolocator.reverse((latitude, longitude))
+
+    # SMS message.
+    text = requester_name + " just fell down and was not responsive!" + \
+           " This happened at " + location.address + ", coordinates " + \
+           latitude + ", " + longitude
+
     to = phone_number + "@txt.att.net"
     smtpObj = smtplib.SMTP_SSL("smtp.gmail.com", 465)
     smtpObj.login("pennapps123@gmail.com", "XZicRmCQGC0f")
-    # email 6266026651@txt.att.net
+
     message = """\
     From: %s
     To: %s
     Subject: text
 
     %s
-    """ % (FROM, ", ".join(to),
-           requester_name + " just fell and may need your help!"
-           + " The distress call came from " + latitude + " " + longitude)
+    """ % (FROM, ", ".join(to), text)
 
-    print "Sending email to", to
-    # smtpObj.sendmail(FROM, to, message)
+    print text
+    # email 6266026651@txt.att.net
+    smtpObj.sendmail(FROM, to, message)
 
-    return("Sending a text..")
+    return("Sending a text to " + to + ", who is at address " + \
+           location.address + " at coordinates " + latitude + ", " + longitude)
 
 if __name__ == "__main__":
     app.run(debug=True)
