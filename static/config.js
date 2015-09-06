2a9dcfc3e58b823c33b061fa77e6514acc5c8c0d
Kevin Liu <nivekuil@ucsd.edu>
2015-09-05 19:31:41 -0700

    Add slider to config

--- static/config.js
+++ static/config.js
@@ -3,7 +3,8 @@ document.body.onload = function() {
   document.getElementById("save").onclick = function() {
     config = {
       'name': document.getElementById("name").value,
-      'contact': document.getElementById("contact").value
+      'contact': document.getElementById("contact").value,
+      'num_passes': document.getElementById("slider").value
     };
     console.log(config);
     location.href = "pebblejs://close#" +
